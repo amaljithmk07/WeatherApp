6bf676f1570d9e9398c22969de60614f540ba3af
amaljithmk07 <amaljithmk123@gmail.com>
2024-03-13 16:45:12 +0530

    url changed

--- front-end/src/components/Constant/BaseUrl.js
+++ front-end/src/components/Constant/BaseUrl.js
@@ -1,5 +1,4 @@
 // const BASE_URI = "http://localhost:2222";
-const BASE_URI =
-  "https://api.render.com/deploy/srv-cnoo0ticn0vc73bi5qg0?key=dqkYb0vd_O0";
+const BASE_URI = "https://weatherapp-uik6.onrender.com";
 
 export default BASE_URI;
