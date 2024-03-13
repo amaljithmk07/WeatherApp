95493188f5d98a52b96666d717135bda0e0db422
amaljithmk07 <amaljithmk123@gmail.com>
2024-03-13 16:20:06 +0530

    url setup

--- front-end/src/components/Constant/BaseUrl.js
+++ front-end/src/components/Constant/BaseUrl.js
@@ -1,3 +1,5 @@
-const BASE_URI = "http://localhost:2222";
+// const BASE_URI = "http://localhost:2222";
+const BASE_URI =
+  "https://api.render.com/deploy/srv-cnoo0ticn0vc73bi5qg0?key=dqkYb0vd_O0";
 
 export default BASE_URI;
